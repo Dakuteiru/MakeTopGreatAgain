f5ad3effb5c250a7faabeeef75f5d361c1fb45f0
Администратор <@GR01>
2026-03-25 21:02:10 +0300

    выфы

--- MakeTopGreatAgain/Data/GroupeCreateRequest.cs
+++ MakeTopGreatAgain/Data/GroupeCreateRequest.cs
@@ -5,6 +5,8 @@ namespace MakeTopGreatAgain.Data;
 
 public class GroupCreateRequest
 {
+    [Required(ErrorMessage ="Необходимо указать название группа")]
+    [MinLength(10, ErrorMessage ="Название группа должно содержать как минимум 10 символов")]
     public required string Title { get; init; }
     public DateTime? startsAt { get; init; }
     public String? TeacherId { get; init; }//
