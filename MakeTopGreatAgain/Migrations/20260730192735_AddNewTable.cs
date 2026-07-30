00c4b398b1026582c670e4d10d125dfdf03b12c3
Evgeniy book <Evgeniy book@DESKTOP-QHKJN4A>
2026-07-30 23:09:08 +0300

    new migration

---
+++ MakeTopGreatAgain/Migrations/20260730192735_AddNewTable.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MakeTopGreatAgain.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNewTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TeacherFile",
+                table: "Homeworks",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "StudentFile",
+                table: "HomeworkCompletions",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TeacherFile",
+                table: "Homeworks");
+
+            migrationBuilder.DropColumn(
+                name: "StudentFile",
+                table: "HomeworkCompletions");
+        }
+    }
+}
